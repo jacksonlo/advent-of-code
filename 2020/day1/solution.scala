3b75ceb5d7369e23046d06b5943b52302ec8f168
Jackson Lo <4205685+jacksonlo@users.noreply.github.com>
2020-12-01 14:31:35 -0500

    day1 part 2

--- 2020/day1/solution.scala
+++ 2020/day1/solution.scala
@@ -2,17 +2,53 @@ import scala.collection.mutable
 import scala.io.Source
 
 object Solution extends App {
-  val target = 2020;
-  val numberToDiff = new mutable.HashMap[Int, Int]();
-  Source.fromResource("input.txt").getLines().map( line => line.toInt).foreach(number => numberToDiff(number) = target - number);
-//  println(numberToDiff);
 
-  var answer: Option[Int] = null;
-  numberToDiff.foreachEntry((number, diff) => {
-    if (numberToDiff.contains(diff)) {
-      answer = Some(number * diff);
-    }
-  });
+  partOne
+  partTwo
 
-  println(answer);
+  def partOne: Unit = {
+    val target = 2020;
+    val numberToDiff = new mutable.HashMap[Int, Int]();
+    Source.fromResource("2020/day1/input.txt").getLines().map(line => line.toInt).foreach(number => numberToDiff(number) = target - number);
+    //  println(numberToDiff);
+
+    var answer: Option[Int] = null;
+    numberToDiff.foreachEntry((number, diff) => {
+      if (numberToDiff.contains(diff)) {
+        answer = Some(number * diff);
+      }
+    });
+
+    println(answer);
+  }
+
+  def partTwo: Unit = {
+    val target = 2020;
+
+    val numbers = Source.fromResource("2020/day1/input.txt").getLines().map(line => line.toInt).toList
+    val numberToDiff = new mutable.HashMap[Int, Int]();
+    numbers.foreach(number => numberToDiff(number) = target - number);
+    //  println(numberToDiff);
+
+    var answer: Option[Int] = null;
+    numberToDiff.foreachEntry((number, diff) => {
+      // Loop over again to find a pairing that adds up to this diff as the target
+      val subTarget = diff;
+
+      val subNumberToDiff = new mutable.HashMap[Int, Int]();
+      numbers.foreach(num => {
+        if (num != number) {
+          subNumberToDiff(num) = subTarget - num
+        }
+      });
+
+      subNumberToDiff.foreachEntry((num2, subDiff) => {
+        if (num2 != subDiff && subNumberToDiff.contains(subDiff)) {
+          answer = Some(number * num2 * subDiff)
+        }
+      })
+    });
+
+    println(answer);
+  }
 }
